97af6319b6a3f981db9e370409f557bb4f0ee237
reyes <reyevj@farmingdale.edu>
2026-04-17 23:26:53 -0400

    init

---
+++ GarmentSimulator/src/main/java/org/example/garmentsimulator/Casual.java
@@ -0,0 +1,14 @@
+package org.example.garmentsimulator;
+
+public class Casual extends Garment{
+    public Casual(){
+        this.topStyle = "Short-sleeve";
+        this.bottomStyle = "Sweatpants";
+        this.shoeStyle = "Sneakers";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ GarmentSimulator/src/main/java/org/example/garmentsimulator/GarmentBuilder.java
@@ -0,0 +1,11 @@
+package org.example.garmentsimulator;
+
+public interface GarmentBuilder {
+    GarmentBuilder setTopStyle(String topStyle);
+    GarmentBuilder setBottomStyle(String bottomStyle);
+    GarmentBuilder setShoeStyle(String leftStyle);
+
+    GarmentBuilder build();
+
+
+}
