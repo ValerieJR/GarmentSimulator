6eeb2f1d6e19e5aa249a8c0dd22214beee410bc5
ValerieJR <157747594+ValerieJR@users.noreply.github.com>
2026-04-17 23:56:01 -0400

    Delete CSC325_JavaFX_OOP directory

--- CSC325_JavaFX_OOP/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.csc325_javafx_oop {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.csc325_javafx_oop to javafx.fxml;
-    exports org.example.csc325_javafx_oop;
-}
